d14a79523113bdee64fc6c97ab181e33c6b4af51
Christopher B. Porch <cbporch@gmail.com>
2015-11-08 11:47:27 -0500

    Added a check for duplicate names.

--- proj2.pl
+++ proj2.pl
@@ -2,6 +2,7 @@
 % Employee name exists in Shifts, which consists
 % of 16 unique names, and is qualified for the job.
 jobs_ok([[A,B,C,D], [E,F,G,H], [I,J,K,L], [M,N,O,P]], [Emp|T]) :-
+	shifts_ok(A, [B,C,D,E,F,G,H,I,J,K,L,M,N,O,P]),
 	employee_ok(Emp,[[A,B,C,D],
 			 [E,F,G,H],
 			 [I,J,K,L],
@@ -11,6 +12,13 @@ jobs_ok([[A,B,C,D], [E,F,G,H], [I,J,K,L], [M,N,O,P]], [Emp|T]) :-
 		 [I,J,K,L],
 		 [M,N,O,P]], T).
 jobs_ok(_Shifts, []).
+
+shifts_ok(_, []).
+shifts_ok(Item, [H|T]) :-
+	Item \== H,
+	shifts_ok(Item, T),
+	shifts_ok(H, T).
+	
 
 % employee_ok(+Employee, +Shifts)
 % Checks that the current employee is in
