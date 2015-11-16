e71939005fc9d058b92a26d43ac5e2bf7beec7bd
Christopher B. Porch <cbporch@gmail.com>
2015-11-16 11:08:28 -0500

    Renamed method.

--- proj2.pl
+++ proj2.pl
@@ -1,8 +1,9 @@
 % jobs_ok(+Shifts, +Employees)
-% Employee name exists in Shifts, which consists
-% of 16 unique names, and is qualified for the job.
+% Checks a list of shifts consisting of Employees
+% and verifies that the given Shifts has no duplicates,
+% that the employees are 'qualified' for their jobs.
 jobs_ok([[A,B,C,D], [E,F,G,H], [I,J,K,L], [M,N,O,P]], [Emp|T]) :-
-	shifts_ok(A, [B,C,D,E,F,G,H,I,J,K,L,M,N,O,P]),
+	no_dup(A, [B,C,D,E,F,G,H,I,J,K,L,M,N,O,P]),
 	employee_ok(Emp,[[A,B,C,D],
 			 [E,F,G,H],
 			 [I,J,K,L],
@@ -13,19 +14,19 @@ jobs_ok([[A,B,C,D], [E,F,G,H], [I,J,K,L], [M,N,O,P]], [Emp|T]) :-
 		 [M,N,O,P]], T).
 jobs_ok(_Shifts, []).
 
-% shifts_ok(+Item, +L)
+% no_dup(+Item, +L)
 % Checks for any duplicates in L.
-shifts_ok(_, []).
-shifts_ok(Item, [H|T]) :-
+% Returns false if there are duplicates.
+no_dup(_, []).
+no_dup(Item, [H|T]) :-
 	Item \== H,
 	shifts_ok(Item, T),
 	shifts_ok(H, T).
 	
-
 % employee_ok(+Employee, +Shifts)
 % Checks that the current employee is in
 % the current shift, and is qualified.
-% Unscheduled employees are accepted as well.
+% Employees not in the shift are accepted as well.
 employee_ok([Name,1,_,_], [[Name,_,_,_]|_]).
 employee_ok([Name,1,_,_], [[_,Name,_,_]|_]).
 employee_ok([Name,_,1,_], [[_,_,Name,_]|_]).
